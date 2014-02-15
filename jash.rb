b7efe2aebab2b379e5ce6f3cdfefba1f33cdb09e
Rohan Prinja <rohan.prinja@gmail.com>
2014-02-15 21:59:08 +0530

    Added .rb extension

--- jash.rb
+++ jash.rb
@@ -3,10 +3,12 @@
 require 'readline'
 require './colorize.rb'
 require './builtins.rb'
+require './signals.rb'
 
 include Builtins
+include SignalHandlers
 
-my_pid = Process.pid
+$my_pid = Process.pid
 
 # read from aliases file
 $aliases = Hash[IO.readlines('aliases.txt').map(&:split)]
@@ -28,37 +30,6 @@ $builtins = Builtins.instance_methods.map &:to_s
 
 $fg_children = []
 $stopped_children = []
-
-trap :INT do
-  if my_pid == Process.pid
-    unless $fg_children.empty?
-      Process.kill :INT, $fg_children.last
-      $fg_children.pop
-    end
-    puts "^C"
-    main
-  end
-end
-
-trap :TSTP do
-  if my_pid == Process.pid
-    unless $fg_children.empty?
-      Process.kill :TSTP, $fg_children.last
-      puts "Stopped: #{fg_children.last}"
-      $stopped_children << $fg_children.pop
-    end
-  end
-end
-
-trap :CONT do
-  if my_pid == Process.pid
-    unless $stopped_children.empty?
-      puts "Resumed: #{stopped_children.last}"
-      Process.kill :CONT, $stopped_children.last
-      $fg_children << $stopped_children.pop
-    end
-  end
-end
 
 $aliases.merge! Hash[bash_aliases]
 
---
+++ signals.rb
@@ -0,0 +1,32 @@
+module SignalHandlers
+  trap :INT do
+    if $my_pid == Process.pid
+      unless $fg_children.empty?
+        Process.kill :INT, $fg_children.last
+        $fg_children.pop
+      end
+      puts "^C"
+      main
+    end
+  end
+
+  trap :TSTP do
+    if $my_pid == Process.pid
+      unless $fg_children.empty?
+        Process.kill :TSTP, $fg_children.last
+        puts "Stopped: #{fg_children.last}"
+        $stopped_children << $fg_children.pop
+      end
+    end
+  end
+
+  trap :CONT do
+    if $my_pid == Process.pid
+      unless $stopped_children.empty?
+        puts "Resumed: #{stopped_children.last}"
+        Process.kill :CONT, $stopped_children.last
+        $fg_children << $stopped_children.pop
+      end
+    end
+  end
+end
