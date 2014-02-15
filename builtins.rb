a6635f266e064be5c2b6a1714350b44638be2d40
Rohan Prinja <rohan.prinja@gmail.com>
2014-02-15 21:06:57 +0530

    separated builtins out into a separate file, add untested ctrl-z functionality

---
+++ builtins.rb
@@ -0,0 +1,23 @@
+module Builtins
+  def cd tokens
+    if tokens.length == 1
+      Dir.chdir # go to home directory
+    else
+      STDERR.puts "Ignoring extra arguments to cd" if tokens.length > 2
+      destination_dir = tokens[1]
+      if destination_dir.eql? '-'
+        destination_dir = $last_dir
+        $last_dir = Dir.pwd
+      end
+      Dir.chdir destination_dir
+    end
+  end
+
+  def pwd tokens
+    Dir.pwd
+  end
+
+  def show_aliases
+    $aliases.each { |k, v| puts "#{k} is aliased to #{v}" }
+  end
+end
--- colorize.rb
+++ colorize.rb
@@ -1,6 +1,5 @@
 class String
-  # colorization
-  def colorize(color_code)
+  def colorize color_code
     "\e[#{color_code}m#{self}\e[0m"
   end
 
